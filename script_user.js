0c932a41ca4c0bf2feb60da6baad96c632c9300d
alexgomezg <55760926+alexgomezg@users.noreply.github.com>
2024-04-02 17:51:59 +0200

    Update script_user.js

--- script_user.js
+++ script_user.js
@@ -610,5 +610,12 @@
 
 
 
+    function getCookie(nombre) {
+        var regex = new RegExp("(?:(?:^|.*;\\s*)" + nombre + "\\s*\\=\\s*([^;]*).*$)|^.*$");
+        var valorCookie = document.cookie.replace(regex, "$1");
+        return decodeURIComponent(valorCookie);
+    }
+
+
     // Your code here...
 })();
