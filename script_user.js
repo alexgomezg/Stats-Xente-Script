b6b9c8f5baf964ed9e51d59a1d9c2b7c2a1a464e
alexgomezg <55760926+alexgomezg@users.noreply.github.com>
2024-07-29 20:57:32 +0200

    Update script_user.js

--- script_user.js
+++ script_user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Stats Xente Script
 // @namespace    http://tampermonkey.net/
-// @version      0.75
+// @version      0.76
 // @description  Stats Xente script for inject own data on Managerzone site
 // @author       xente
 // @match        https://www.managerzone.com/*
